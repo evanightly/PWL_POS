2a843634653cc28e94b863d7d411048c961902cf
Galur Arasy <adamhenderson3x3@gmail.com>
2024-05-17 08:05:32 +0700

    Update pertemuan 11 - praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -19,6 +20,7 @@ class UserModel extends Authenticatable implements JWTSubject {
         'username',
         'nama',
         'password',
+        'image'
     ];
 
     public function getJWTIdentifier() {
@@ -31,5 +33,11 @@ class UserModel extends Authenticatable implements JWTSubject {
 
     public function level(): BelongsTo {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    public function image(): Attribute {
+        return Attribute::make(
+            get: fn ($image) => url('storage/posts/' . $image),
+        );
     }
 }
